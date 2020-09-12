5e6db8f64e5d46ce38dc55f6ff06b6e4357cfda8
xAJBx <aj@bridgesautomation.com>
2020-09-12 15:54:23 -0500

    made data endpoit reffrence stored procedure

--- routes/data.js
+++ routes/data.js
@@ -6,10 +6,17 @@ const request = require('request');
 // @route   POST api/data
 // @desc    Insert Sensor data to local db & relays post request to datacenter
 // @access  Public
-Router.post("/:Coordinates/:Sensor0/:Sensor1/:Sensor2/:Sensor3/:ErrorCode", (req, res) => {
-    mysqlConnection.query(`INSERT INTO sensor_data (Coordinates, Sensor0_data, Sensor1_data, Sensor2_data, Sensor3_data, ErrorCode) 
-    VALUES ('${req.params.Coordinates}' , '${req.params.Sensor0}', '${req.params.Sensor1}', '${req.params.Sensor2}', '${req.params.Sensor3}', '${req.params.ErrorCode}')`);  
-    res.send(x);
-})
+Router.post("/:unit_id/:gps_coord/:sensor_id/:sensor_reading", async (req, res) => {
+    console.log('error api')
+    try{
+        
+        mysqlConnection.query(`CALL spINSERT_DATA('${req.params.unit_id}', '${req.params.gps_coord}', '${req.params.sensor_id}', '${req.params.sensor_reading}')`);
+    
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+    res.send('done');
+})//
 
 module.exports = Router;
